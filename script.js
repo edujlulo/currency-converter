c1df8073e5641fda802df76bd56057dd64fb7367
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-08-08 22:31:13 +0100

    Updates

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-// const buttonCurrency1 = document.getElementById("button-currency1");
-// const buttonCurrency2 = document.getElementById("button-currency2");
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
-
-// buttonCurrency1.addEventListener("click", function () {
-//   input1.value = parseFloat((input2.value * 0.8664760419374404).toFixed(2));
-// });
-
-// buttonCurrency2.addEventListener("click", function () {
-//   input2.value = parseFloat((input1.value * 1.1541).toFixed(2));
-// });
 
 const menu1 = document.getElementById("menu-1");
 const menu2 = document.getElementById("menu-2");
 
 const originalOptions = [
   { value: "pound-sterling", text: "Pound Sterling" },
-  { value: "us-dollar", text: "US Dollar" },
   { value: "euro", text: "Euro" },
+  { value: "us-dollar", text: "US Dollar" },
   { value: "japanese-yen", text: "Japanese Yen" },
 ];
 
@@ -47,3 +37,11 @@ menu2.addEventListener("change", () => updateMenus(menu2, menu1));
 
 updateMenus(menu1, menu2);
 updateMenus(menu2, menu1);
+
+// Calculation of exchange
+
+input1.addEventListener("input", calculationInput2);
+
+function calculationInput2() {
+  input2.value = input1.value * 1.1541;
+}
